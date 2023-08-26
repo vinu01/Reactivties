873e6016742c642f8798e024d847370a7bf517d9
Vinu01 <vinodkumar15190@gmail.com>
2023-08-27 01:16:19 +0530

    Api implement

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,7 @@
+
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
@@ -19,5 +28,18 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+using var scope=app.Services.CreateScope();
+var services=scope.ServiceProvider;
+try
+{
+    var context=services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger=services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex,"An Error occured during migration");
+}
 
 app.Run();
